7f0c7a923dd119a51c5cd257b4b754391b2fa044
Carlosmzh <a2024112447@estudiantes.upsa.edu.bo>
2025-10-25 00:54:31 -0400

    Create Auth Dto

---
+++ Dtos/Auth/AuthResponseDto.cs
@@ -0,0 +1,11 @@
+﻿using EcommerceApi.DTOs.Users;
+
+namespace EcommerceApi.DTOs.Auth
+{
+    public class AuthResponseDto
+    {
+        public string Token { get; set; } = null!;
+        public DateTime Expiration { get; set; }
+        public UserResponseDto User { get; set; } = null!;
+    }
+}
